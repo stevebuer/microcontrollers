0607befdd8346f6ec02297168f8dc68ecdddc152
Steve <steve@jxqz.org>
2023-04-12 20:44:55 -0700

    Add script to check if bmp180 attached to i2c bus

---
+++ bmp180/README.md
@@ -0,0 +1,18 @@
+# Adafruit Bosch BMP180 breakout
+
+I purchased this board in 2014 or 2015. 
+
+It has been obsoleted by a newer version and does not seems to be supported in CircuitPython.
+
+However, I would still like to be able to use it as part of my weather station.
+
+### Data Sheet
+
+[Bosch BMP180 Data Sheet](https://cdn-shop.adafruit.com/datasheets/BST-BMP180-DS000-09.pdf)
+
+### Code
+
+	* Makefile - scan the i2c bus
+	* bmp180-check.py - Use pyftdi to query the device via FT232H MPSSE
+
+To Do
---
+++ bmp180/bmp180-check.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+# 
+# Check BMP180 device is connected
+# Steve Buer, N7MKO
+# 
+
+import sys
+import pyftdi 
+
+from pyftdi.i2c import I2cController
+
+# Check communication with BMP180 i2c chip
+
+FTDI_DEVICE='ftdi://ftdi:232h/1'
+BMP180_I2CADDR=0x77
+BMP180_ID_REGISTER=0xD0
+
+print("opening device:", FTDI_DEVICE)
+
+try:
+
+    i2c = I2cController()
+
+    i2c.configure(FTDI_DEVICE)
+
+except pyftdi.usbtools.UsbToolsError as e:
+
+    print("error:", e)
+    sys.exit(1)
+
+slave = i2c.get_port(BMP180_I2CADDR)
+
+print("checking BMP180 register: %x at address: %x" % (BMP180_ID_REGISTER, BMP180_I2CADDR))
+
+try:
+
+    slave.write([0xD0])
+
+    result = slave.exchange([0xD0], 1)
+
+except pyftdi.i2c.I2cNackError as e:
+
+    print("error:", e)
+    sys.exit(2)
+
+print("read value:", result.hex())
