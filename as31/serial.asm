3b4e653fbadcd62a72a9b16ba962773d2e199019
Steve Buer <stephenbuer@student.olympic.edu>
2025-05-13 16:28:57 -0700

    read/write serial port for single letter command

---
+++ as31/serial.asm
@@ -0,0 +1,32 @@
+;
+; 8051 serial port write
+;
+; AS31 assembler
+;
+
+.EQU CMD, 080H
+
+; Interrupt vectors
+
+.ORG 0000H
+	LJMP BEGIN
+
+; Main
+
+.ORG 40
+
+BEGIN:
+	MOV R0, CMD
+        MOV TMOD, #00100000B
+	MOV TH1, #11111101B
+	MOV SCON, #1010000B
+	MOV TR1, #1
+
+PROMPT: MOV SBUF, #'>'
+	CLR TI 
+WAIT:	JNB TI, WAIT
+
+READ:	JNB RI, READ
+	MOV A, SBUF
+	MOV @R0, A
+	AJMP PROMPT
