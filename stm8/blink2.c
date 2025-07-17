c5a4a5f858104a0fd849d132e70982d1a9411d6f
Steve Buer <stephenbuer@student.olympic.edu>
2025-07-16 19:54:35 -0700

    blink faster, simpler code

---
+++ stm8/blink2.c
@@ -0,0 +1,39 @@
+/*
+ * BLink Test LED on STM8S003F3P6 eBay development board
+ *
+ * compile: sdcc -mstm8 led-test.c
+ *
+ * Steve Buer
+ * July 2025
+ */
+
+/* LED is on Port B pin 5 */
+
+#define PIN5 (1 << 5)
+
+/* pointers to port B registers */
+
+volatile unsigned char * PORTB_ODR = 0x5005;
+volatile unsigned char * PORTB_DDR = 0x5007;
+volatile unsigned char * PORTB_CR1 = 0x5008;
+
+int main() 
+{
+	/* Port B data direction register (DDR) pin 5 output */
+
+	*PORTB_DDR = PIN5;
+
+	/* Port B configuration register (CR1) pin 5 push-pull */ 
+	
+	*PORTB_CR1 = PIN5;
+
+	while (1) {
+
+		/* Port B output data register (ODR) toggle */
+
+		*PORTB_ODR ^= PIN5; 
+	
+		for (unsigned long n = 100000L; n > 0; n--)
+			__asm__("nop");
+	}
+}
