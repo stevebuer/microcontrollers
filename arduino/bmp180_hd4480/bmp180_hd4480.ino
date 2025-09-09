bcef5a1652b2d66bca417b5f8d524ab4b8699e4f
Steve <87083511+stevebuer@users.noreply.github.com>
2025-09-09 15:02:41 -0700

    Update bmp180_hd4480.ino

--- arduino/bmp180_hd4480/bmp180_hd4480.ino
+++ arduino/bmp180_hd4480/bmp180_hd4480.ino
@@ -24,15 +24,24 @@
 
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 BMP180 bmp180;
+//ImperialSystem imperial;
 
 void setup() 
 {
   Serial.begin(9600);
   lcd.begin(COLUMNS, ROWS);
+  //bmp180.setMetricSystem(imperial);
+  
   if (!bmp180.begin())
     lcd.print("BMP180 not found!");
   else
     lcd.print("BMP180");
+}
+
+void read_altitude()
+{
+  Serial.print(bmp180.readAltitude());
+  Serial.print(" m");
 }
 
 void read_cmd()
@@ -44,27 +53,31 @@ void read_cmd()
 
   switch (command) {
 
-    case 'p':
-       // read_pressure();
+    case 'a':
+       read_altitude();
        break;
-
-    case 't':
-      // read_temp();
-      break;
 
     case 'h':
     default:
-      Serial.println("g = start graphing, p = read pressure, t = read temp");
+      Serial.println("g = start graphing, a = read altitude, p = read pressure, t = read temp");
   }
 
   Serial.print("ok ");
 }
 
 void loop()
-{  
+{
+    lcd.setCursor(10,0);
+    lcd.print(bmp180.readAltitude());
     lcd.setCursor(0,1);
     lcd.print(bmp180.readTemperature());
-    lcd.print(" C");
+    lcd.print("  ");
+    lcd.print(bmp180.readPressure());
+    lcd.print(" ");
 
-    delay(2000);
+    read_cmd();
+    
+    delay(5000);
 }
+
+
