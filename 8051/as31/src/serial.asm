5f20f73ca8b01f4ca90756103a9186e5b4048260
Steve Buer <steve@jxqz.org>
2026-04-01 09:14:25 -0700

    adjust formatting

--- 8051/as31/src/serial.asm
+++ 8051/as31/src/serial.asm
@@ -10,22 +10,28 @@
 
 .ORG 30H
 
-BEGIN:	MOV SCON, #50H		; mode 1, REN = 1
+BEGIN:	
+	MOV SCON, #50H		; mode 1, REN = 1
 	MOV TMOD, #20H		; timer mode 2
 	MOV TH1, #0FDH		; 9600 baud @ 11.0592 MHz
 	SETB TR1		; start timer 1
 
 	MOV R0, #LINE		; read buffer
 
-PROMPT: MOV SBUF, #'>'
-WAIT:   JNB TI, WAIT
+PROMPT: 
+	MOV SBUF, #'>'
+WAIT:   
+	JNB TI, WAIT
 	CLR TI
 
-READ:	JNB RI, READ
+READ:	
+	JNB RI, READ
 	MOV A, SBUF
 	CLR RI
 	MOV @R0, A
 	INC R0
 	CJNE R0, #LINE+SIZE, NEXT
-WRAP:	MOV R0, #LINE		; wrap buffer
-NEXT:	SJMP READ
+WRAP:	
+	MOV R0, #LINE		; wrap buffer
+NEXT:	
+	SJMP READ
